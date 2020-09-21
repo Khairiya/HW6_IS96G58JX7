b2dda4701b0b118848ab4ad60651ef3a79f07a35
unknown <rashagibrill@gmail.com>
2020-09-21 03:41:41 +0000

    Updates

---
+++ precache-manifest.706041ead5e86c5d62f5464645032415.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c8a179ace52e9e029a64d9b6f718c61",
+    "url": "/HW6_IS96G58JX7/index.html"
+  },
+  {
+    "revision": "2da5b7e1d759a5c2a67c",
+    "url": "/HW6_IS96G58JX7/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "714359ac221eb521c890",
+    "url": "/HW6_IS96G58JX7/static/js/2.7d72e063.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/HW6_IS96G58JX7/static/js/2.7d72e063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2da5b7e1d759a5c2a67c",
+    "url": "/HW6_IS96G58JX7/static/js/main.5d5ce31c.chunk.js"
+  },
+  {
+    "revision": "a4295f83fb11e6c70398",
+    "url": "/HW6_IS96G58JX7/static/js/runtime-main.8aa31330.js"
+  }
+]);
